e64612f3ca68391b33fb6e56aff46ee070ab4ce0
tktk0430 <h.taku.0430.tech@gmail.com>
2022-08-07 02:31:33 +0900

    フォームガワ

---
+++ src/AnswerForm.tsx
@@ -0,0 +1,11 @@
+const AnswerForm = () => {
+  return (
+    <>
+      <div className="answer-note">カタカナで</div>
+      <input className="answer-input" />
+      <div className="answer-button">Answer</div>
+    </>
+  );
+};
+
+export { AnswerForm };
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { AnswerForm } from "./AnswerForm";
 import "./App.css";
 import { Question } from "./Question";
 
 function App() {
   return (
     <div className="main">
-      <div className="header">SAKUMONDO</div>
+      <div className="header">
+        <div style={{ fontSize: "1.5rem" }}>SAKUMONDO</div>
+      </div>
       <div className="body">
         <Question />
+        <AnswerForm />
       </div>
     </div>
   );
