066bafc0bcd6efcab19a3e45882212cf794341f5
tktk0430 <h.taku.0430.tech@gmail.com>
2022-08-08 23:45:15 +0900

    遊び方モーダル作成

--- src/pages/Footer/main.tsx
+++ src/pages/Footer/main.tsx
@@ -1,16 +1,52 @@
+import { Button } from "components/Button";
+import { Modal } from "components/Modal";
 import gitIcon from "images/github.png";
+import { useState } from "react";
 
 const Footer = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div className="external-links">
-      <a
-        href="https://github.com/tktk0430/sakumondo"
-        target="_blank"
-        rel="noopener noreferrer"
+    <>
+      <div
+        className="flex-center"
+        style={{
+          marginTop: "1rem",
+          textDecoration: "underline",
+          textUnderlineOffset: "0.2rem",
+        }}
       >
-        <img src={gitIcon} alt="github link" />
-      </a>
-    </div>
+        <Button
+          style={{ cursor: "pointer" }}
+          onClick={() => setIsModalOpen(true)}
+        >
+          遊び方
+        </Button>
+      </div>
+      <div className="external-links">
+        <a
+          href="https://github.com/tktk0430/sakumondo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={gitIcon} alt="github link" />
+        </a>
+      </div>
+      <Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)}>
+        <div className="red" style={{ fontSize: "2rem", textAlign: "center" }}>
+          遊び方
+        </div>
+        <ol style={{ lineHeight: "1.5rem" }}>
+          <li>パネルをタップして問題文の文字を開きます。</li>
+          <li>
+            クイズの答えが分かったら、解答欄に入力して「Answer」をタップしてください。
+            3回まで解答できます。
+          </li>
+          <li>
+            開かなかった文字数が得点になります。できるだけ文字を開けずにクイズに正解しましょう!
+          </li>
+        </ol>
+      </Modal>
+    </>
   );
 };
 
