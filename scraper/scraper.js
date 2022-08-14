0de89545ba644a26eeeffc3cdfea202497831715
tktk0430 <h.taku.0430.tech@gmail.com>
2022-08-14 23:08:27 +0900

    ittan

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,10 +1,17 @@
+import dayjs from "dayjs";
+import { encode } from "../src/utils/crypto";
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
 var isSucceeded = false;
-
-(async () => {
-  const today = process.argv[2];
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const INSTRUCTION_TO_ANSWER_TYPE = {
+  カタカナで: "katakana",
+  数字で: "number",
+};
+const hoge = async (date) => {
+  console.log(date, "date");
+  let result = null;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setRequestInterception(true);
@@ -25,18 +32,50 @@ var isSucceeded = false;
     try {
       const json = JSON.parse(consoleObj.text());
       if (json.date === 0 || isSucceeded) return;
-      console.log("日付", json.date);
-      console.log("問題", json.question.join(""));
-      console.log("形式", json.instruction);
-      console.log("解答", json.answers.join(", "));
       isSucceeded = true;
+      result = json;
     } catch {}
   });
-  await page.goto(`https://mondo.quizknock.com/?date=${today}&indices=0`);
-  await page.screenshot({ path: "example.png" });
+  await page.goto(`https://mondo.quizknock.com/?date=${date}&indices=0`);
+  await page.waitForTimeout(2000);
 
   await browser.close();
-})();
+  return result;
+};
+
+const fuga = async (date) => {
+  const result = await hoge(date);
+  const question = result
+    ? {
+        sentence: result.question.join(""),
+        answerType: INSTRUCTION_TO_ANSWER_TYPE[result.instruction],
+        answers: result.answers.join("\n"),
+      }
+    : null;
+
+  if (question) {
+    console.log(date, result);
+    const encoded = encode(question);
+    return { [date]: encoded };
+  }
+  return {};
+};
+
+const piyo = async (start, end) => {
+  const kakomon = JSON.parse(fs.readFileSync("scraper/kakomon.json", "utf-8"));
+  const existingDays = Object.keys(kakomon);
+
+  const startDate = dayjs(start);
+  const endDate = dayjs(end);
+
+  for (let date = startDate; date <= endDate; date = date.add(1, "day")) {
+    const target = date.format("YYYY-MM-DD");
+    if (target in existingDays) continue;
+    const result = await fuga(target);
+    await sleep(5000);
+  }
+};
+piyo("2022-05-19", "2022-05-21");
 
 //   page.on("response", async (response) => {
 //     const url = response.url();
