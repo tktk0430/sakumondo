0a6c6c91381480ce5dff579732aaf15d6c224da0
tktk0430 <h.taku.0430.tech@gmail.com>
2022-08-17 01:20:49 +0900

    解いた過去問に色を付ける

--- src/pages/Kakomon/main.tsx
+++ src/pages/Kakomon/main.tsx
@@ -2,9 +2,10 @@ import { getKakomonURL } from "api/gas";
 import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { getResult, ResultType } from "utils/localStorage";
 
 export const Kakomon = () => {
-  const [kakomon, setKakomon] = useState<{ [key: string]: string[] }>({});
+  const [kakomon, setKakomon] = useState<{ [key: string]: string }>({});
 
   useEffect(() => {
     const fetchOriginURL = async () => {
@@ -30,6 +31,10 @@ export const Kakomon = () => {
     );
   }, [kakomon]);
 
+  if (Object.keys(kakomon).length === 0) {
+    return <div>読み込み中...</div>;
+  }
+
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>過去問</h1>
@@ -48,31 +53,70 @@ export const Kakomon = () => {
               {dates.map((day) => {
                 const key = `${yearMonth}-${day}`;
                 return (
-                  <div
-                    key={key}
-                    style={{
-                      textAlign: "center",
-                      outline: "solid 0.5px",
-                      height: "3.5rem",
-                    }}
-                  >
-                    {key in kakomon ? (
-                      <Link
-                        to={`/?q=${kakomon[key]}`}
-                        style={{ textDecoration: "none" }}
-                      >
-                        {Number(day)}
-                      </Link>
-                    ) : (
-                      <span></span>
-                    )}
-                  </div>
+                  <DatePanel
+                    isExist={key in kakomon}
+                    q={kakomon[key]}
+                    day={Number(day)}
+                    result={getResult(decodeURIComponent(kakomon[key]))}
+                  />
                 );
               })}
             </div>
           </div>
         );
       })}
+    </div>
+  );
+};
+
+type DatePanleProps = {
+  isExist: boolean;
+  q: string;
+  day: number;
+  result: ResultType;
+};
+
+const DatePanel = (props: DatePanleProps) => {
+  const { isExist, q, day, result } = props;
+  const isSolving = result.clickedIndices.length > 0;
+
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        outline: "solid 0.5px gray",
+        height: "3.5rem",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: "0.2rem",
+        backgroundColor: result.isCorrect ? "#5eec62" : "white",
+        position: "relative",
+      }}
+    >
+      {isExist ? (
+        <Link
+          to={`/?q=${q}`}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          {day}
+        </Link>
+      ) : (
+        <span></span>
+      )}
+      {!result.isCorrect && isSolving && (
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 2,
+            fontSize: 0.5,
+          }}
+          className="red"
+        >
+          ●
+        </div>
+      )}
     </div>
   );
 };
